ac2cb378d67bd520fff6a82b67bfa1436df74f42
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2021-03-06 15:15:36 -0300

    ccheck v2

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     
-  const asiatica = new Swiper('.swiper-asiatica', {
-    loop: true
+  // SWIPERS
+    const asiatica = new Swiper('.swiper-asiatica', {
+      loop: true
     });
     const carnes = new Swiper('.swiper-carnes', {
       loop: true
@@ -12,33 +13,68 @@ $(document).ready(function () {
     const pinchos = new Swiper('.swiper-pinchos', {
       loop: true
     });
+
+    const papas = new Swiper('.swiper-papas', {
+      loop: true
+    });
+
+    const helados = new Swiper('.swiper-helados', {
+      loop: true
+    });
+
+    const coct = new Swiper('.swiper-coct', {
+      loop: true
+    });
     
-    $( ".swiper-asiatica" ).mouseenter(function() {
+
+
+    //EVENTS
+    $(".swiper-asiatica").mouseenter(function() {
         asiatica.autoplay.start();
     });
-    $( ".swiper-asiatica" ).mouseleave(function() {
+    $(".swiper-asiatica").mouseleave(function() {
       asiatica.autoplay.stop();
     });
     
-    $( ".swiper-carnes" ).mouseenter(function() {
+    $(".swiper-carnes").mouseenter(function() {
       carnes.autoplay.start();
     });
-    $( ".swiper-carnes" ).mouseleave(function() {
+    $(".swiper-carnes").mouseleave(function() {
       carnes.autoplay.stop();
     });
 
-    
-    $( ".swiper-pizza" ).mouseenter(function() {
+    $(".swiper-pizza").mouseenter(function() {
       pizza.autoplay.start();
     });
-    $( ".swiper-pizza" ).mouseleave(function() {
+    $( ".swiper-pizza").mouseleave(function() {
       pizza.autoplay.stop();
     });
 
-    $( ".swiper-pinchos" ).mouseenter(function() {
+    $(".swiper-pinchos").mouseenter(function() {
       pinchos.autoplay.start();
     });
-    $( ".swiper-pinchos" ).mouseleave(function() {
+    $(".swiper-pinchos").mouseleave(function() {
       pinchos.autoplay.stop();
+    });
+
+    $(".swiper-papas").mouseenter(function() {
+      papas.autoplay.start();
+    });
+    $(".swiper-papas" ).mouseleave(function() {
+      papas.autoplay.stop();
+    });
+
+    $(".swiper-helados").mouseenter(function() {
+      helados.autoplay.start();
+    });
+    $(".swiper-helados").mouseleave(function() {
+      helados.autoplay.stop();
+    });
+
+    $(".swiper-coct").mouseenter(function() {
+      coct.autoplay.start();
+    });
+    $(".swiper-coct").mouseleave(function() {
+      coct.autoplay.stop();
     });
 })
