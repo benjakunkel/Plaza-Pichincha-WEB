5c496de16d291e37207f5f1bf2ebabf33a2bc361
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2021-03-06 16:12:57 -0300

    sdafljbasd;uhf

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -5,12 +5,14 @@ $(document).ready(function () {
       loop: true
     });
     const carnes = new Swiper('.swiper-carnes', {
-      loop: true
+      loop: true,
+      speed: 1000,
     });
     const pizza = new Swiper('.swiper-pizza', {
       loop: true
     });
     const pinchos = new Swiper('.swiper-pinchos', {
+      autoResize:true,
       loop: true
     });
 
@@ -29,52 +31,76 @@ $(document).ready(function () {
 
 
     //EVENTS
+
+    //asiatica
     $(".swiper-asiatica").mouseenter(function() {
-        asiatica.autoplay.start();
+      asiatica.autoplay.start();
+      $(".swiper-asiatica .swiper-title-1-r").addClass("hidden");
     });
     $(".swiper-asiatica").mouseleave(function() {
       asiatica.autoplay.stop();
+      $(".swiper-asiatica .swiper-title-1-r").removeClass("hidden");
     });
     
+    //carnes
     $(".swiper-carnes").mouseenter(function() {
       carnes.autoplay.start();
+      $(".swiper-carnes .swiper-title-1-r").addClass("hidden");
     });
     $(".swiper-carnes").mouseleave(function() {
       carnes.autoplay.stop();
+      $(".swiper-carnes .swiper-title-1-r").removeClass("hidden");
     });
+    
 
+    //pizza
     $(".swiper-pizza").mouseenter(function() {
       pizza.autoplay.start();
-    });
-    $( ".swiper-pizza").mouseleave(function() {
+      $(".swiper-pizza .swiper-title-2-r").addClass("hidden");
+    }); 
+    $(".swiper-pizza").mouseleave(function() {
       pizza.autoplay.stop();
+      $(".swiper-pizza .swiper-title-2-r").removeClass("hidden");
     });
+    
 
+    //pinchos
     $(".swiper-pinchos").mouseenter(function() {
       pinchos.autoplay.start();
+      $(".swiper-pinchos .swiper-title-1-r").addClass("hidden");
     });
     $(".swiper-pinchos").mouseleave(function() {
       pinchos.autoplay.stop();
+      $(".swiper-pinchos .swiper-title-1-r").removeClass("hidden");
     });
 
+     //papas
     $(".swiper-papas").mouseenter(function() {
       papas.autoplay.start();
+      $(".swiper-papas .swiper-title-2-r").addClass("hidden");
     });
-    $(".swiper-papas" ).mouseleave(function() {
+    $(".swiper-papas").mouseleave(function() {
       papas.autoplay.stop();
+      $(".swiper-papas .swiper-title-2-r").removeClass("hidden");
     });
 
+    //helados
     $(".swiper-helados").mouseenter(function() {
       helados.autoplay.start();
+      $(".swiper-helados .swiper-title-2-r").addClass("hidden");
     });
     $(".swiper-helados").mouseleave(function() {
       helados.autoplay.stop();
+      $(".swiper-helados .swiper-title-2-r").removeClass("hidden");
     });
 
+    //coct
     $(".swiper-coct").mouseenter(function() {
       coct.autoplay.start();
-    });
+      $(".swiper-coct .swiper-title-1-r").addClass("hidden");
+    }); 
     $(".swiper-coct").mouseleave(function() {
       coct.autoplay.stop();
+      $(".swiper-coct .swiper-title-1-r").removeClass("hidden");
     });
 })
